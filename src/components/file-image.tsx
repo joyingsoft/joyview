3cdd5a4be97140fd44365f7be05ba8a80395f8eb
GuoJunjun <guojunjun@gmail.com>
2021-12-24 12:44:37 +0100

    reduce image size with canvas

--- src/components/file-image.tsx
+++ src/components/file-image.tsx
@@ -1,14 +1,40 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 
 export const FileImage: FC<{ file: File; classNames?: string }> = ({
   file,
   classNames,
 }) => {
-  return (
-    <img
-      className={classNames}
-      src={URL.createObjectURL(file)}
-      alt={file.name}
-    />
-  );
+  const [data, setData] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    const resizeRate = 0.2;
+    const imgFile = new Image();
+    imgFile.src = URL.createObjectURL(file);
+
+    imgFile.onload = () => {
+      const oc = document.createElement('canvas');
+      oc.height = imgFile.height * resizeRate;
+      oc.width = imgFile.width * resizeRate;
+      const octx = oc.getContext('2d');
+      if (octx) {
+        octx.drawImage(
+          imgFile,
+          0,
+          0,
+          imgFile.width * resizeRate,
+          imgFile.height * resizeRate,
+        );
+
+        setData(oc.toDataURL(file.type));
+      } else {
+        console.log('An error has occurred!');
+      }
+    };
+  }, [file]);
+
+  if (data && file) {
+    return <img className={classNames} src={data} alt={file.name} />;
+  }
+
+  return null;
 };
