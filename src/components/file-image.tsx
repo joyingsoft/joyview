8e4b2f008063f7f4e42e93481b0fe93202bdb389
GuoJunjun <guojunjun@gmail.com>
2021-12-25 23:30:27 +0100

    impl resize large png, jpeg, webp images

--- src/components/file-image.tsx
+++ src/components/file-image.tsx
@@ -1,4 +1,9 @@
-import { FC, useEffect, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
+import {
+  getResizedDataURL,
+  isDefinedResizeType,
+  loadImageFromFile,
+} from '../utils/img-resize';
 
 export const FileImage: FC<{ file: File; classNames?: string }> = ({
   file,
@@ -7,28 +12,19 @@ export const FileImage: FC<{ file: File; classNames?: string }> = ({
   const [data, setData] = useState<string | undefined>(undefined);
 
   useEffect(() => {
-    const resizeRate = 0.2;
-    const imgFile = new Image();
-    imgFile.src = URL.createObjectURL(file);
-
-    imgFile.onload = () => {
-      const oc = document.createElement('canvas');
-      oc.height = imgFile.height * resizeRate;
-      oc.width = imgFile.width * resizeRate;
-      const octx = oc.getContext('2d');
-      if (octx) {
-        octx.drawImage(
-          imgFile,
-          0,
-          0,
-          imgFile.width * resizeRate,
-          imgFile.height * resizeRate,
-        );
-
-        setData(oc.toDataURL(file.type));
-      } else {
-        console.log('An error has occurred!');
-      }
+    if (isDefinedResizeType(file.type)) {
+      loadImageFromFile(file).then((img) => {
+        if (img.height > 1000 || img.width > 1000) {
+          setData(getResizedDataURL(img));
+        } else {
+          setData(img.src);
+        }
+      });
+    } else {
+      setData(URL.createObjectURL(file));
+    }
+    return () => {
+      setData('');
     };
   }, [file]);
 
